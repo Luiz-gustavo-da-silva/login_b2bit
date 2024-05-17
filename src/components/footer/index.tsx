c2358e64f41751278151360e53eeb1dfa7c01b3b
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-05-17 16:26:37 -0300

    [Luiz] refactor: componentizando o footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      Feito por <a href="https://github.com/Luiz-gustavo-da-silva" target="_blank">Luiz Gustavo</a>
+    </footer>
+  );
+};
+
+export default Footer;
