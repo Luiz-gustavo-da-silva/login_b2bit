d20815288fd639f96290d5409fc419e6e1191c89
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-05-17 22:05:11 -0300

    [Luiz] refactor: remoção de código obsoleto

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./styles.module.css";
 import Button from "../Button";
 import { useNavigate } from "react-router-dom";
--- src/routes/protectedRoutes.tsx
+++ src/routes/protectedRoutes.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import UserServices from "../services/UserService";
 import { Navigate } from "react-router-dom";
 
